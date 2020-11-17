0eb25d4760600e999f9515a372842752fd0b0246
Adriano Salles <adrianorsalles@hotmail.com>
2020-11-17 11:04:07 -0300

    feat: add bar chart

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 
+import covid from './images/covid.png';
+
 import { fetchData } from './api';
 
 class App extends React.Component {
@@ -19,13 +21,14 @@ class App extends React.Component {
   };
 
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
 
     return (
       <div className={styles.container}>
+        <img className={styles.image} src={covid} alt="COVID 19" />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart />
+        <Chart data={data} country={country} />
       </div>
     );
   }
